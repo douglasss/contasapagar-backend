2edae05f1e1fdbb178a97a5634e7874a713a02e3
Douglas <douglas.souza.93.ds@gmail.com>
2021-09-23 10:32:39 -0300

    Commit inicial

---
+++ src/main/java/com/douglasss/contasapagarbackend/ContasapagarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.douglasss.contasapagarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasapagarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasapagarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglasss/contasapagarbackend/ContasapagarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasss.contasapagarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasapagarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
